267ff992a83adddd3de4e39000a4c703f8a40b0d
Max Pothier <tarneaux>
2021-06-20 15:22:48 +0200

    fix start_minecraft.py

--- start_minecraft.py
+++ start_minecraft.py
@@ -83,7 +83,7 @@ class Minecraft:
     def socket_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind(("127.0.0.1", 25556))
+            s.bind(("", 25556))
             try:
                 while True:
                     s.listen()
